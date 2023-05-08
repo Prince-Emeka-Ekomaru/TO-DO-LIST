354906a59068df98b350925f551f89756de1d2d0
Prince Emeka <princeekomaru@gmail.com>
2023-05-08 16:49:57 +0100

    .

--- script.js
+++ script.js
@@ -55,3 +55,62 @@ ul.addEventListener('click', handleUIClick);
 button.addEventListener("click", addListAfterClick);
 
 input.addEventListener("keypress", addListAfterKeypress);
+
+const Todos = async () => {
+	const response = await fetch ('eunice.json')
+	if (response.status !== 200){
+		throw new Error('this is an error response');
+	}
+	const data = await response.json()
+	return data;
+}
+
+Todos()
+.then((data) => console.log('Resolved:', data))
+.catch((err) => console.log('Rejected:', err.message));
+
+// fetch('eunice.json').then((response) => {
+// 	console.log('resolved',response)
+// 	return response.json();
+// }).then((data) => {
+// 	console.log('data',data)
+// }).catch((err) => {
+// 	console.log('err',err)
+// })
+
+
+
+
+
+// const Todos = (resource) => {
+// 	return new Promise((resolve, reject) => {
+
+// 		const request = new XMLHttpRequest();
+
+// request.addEventListener('readystatechange', () => {
+// 	if (request.readyState === 4 && request.status === 200) {
+// 		const data = JSON.parse(request.responseText);
+// 		resolve(data);
+// 	} else if(request.readyState === 4){
+// 		reject('error getting you message');
+// 	}
+// })
+
+// request.open('GET',resource);
+// request.send();
+
+
+// 	})
+// }
+
+// Todos('tiannah.json').then(data => {
+// 	console.log('promise 1 resolved:', data);
+// 	return Todos('eunice.json');
+// }).then(data => {
+// 	console.log('promise 2 resolved:', data);
+// 	return Todos('prince.json');
+// }).then(data => {
+// 	console.log('promise 3 resolved:', data);
+// }).catch(err =>{
+// 	console.log('promise rejected:', err)
+// })
